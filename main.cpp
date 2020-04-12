530b7e09adc2fea15bab4e15f5d44b94d8f606d1
grishashvili <P0llaris!>
2020-04-12 04:48:23 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello, World!" << endl;
+
+    for ( int i=0; i<10; i++ ) {
+
+        cout << "zhopa noviy god" << endl << flush;
+
+    }
+
+    cout << "done" << endl;
+
+    return 0;
+}
